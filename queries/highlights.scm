f64f9f9bfd09b409d7d6bd550747cc178a35ae09
Garfieldcmix <chayanon.nin@gmail.com>
2025-06-12 17:50:54 +0700

    Fix highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,78 +1,166 @@
 ;;; Highlighting for MiniScript
-;;; TODO: Fix Highlighting
 
-;;; Keywords
+;; Keywords
 
-["if" "then" "else" "end if"] @keyword.conditional
-["while" "end while"] @keyword.repeat
-["for" "in" "end for"] @keyword.repeat
-[(break_statement) (continue_statement)] @keyword.repeat
-["return"] @keyword.return
-["function" "end function"] @keyword.function
-["not" "and" "or" "isa"] @keyword.operator
-["new"] @keyword
+"new" @keyword
 
-;;; Operators
+"return" @keyword.return
+
+(break_statement) @keyword
+
+(continue_statement) @keyword
+
+(if_statement
+[
+  "if"
+  "else"
+  "then"
+  "end if"
+] @keyword.conditional)
+
+(if_statement_shorthand
+[
+  "if"
+  "else"
+  "then"
+] @keyword.conditional)
+
+(elseif_statement
+[
+  "else if"
+  "then"
+  "end if"
+] @keyword.conditional)
+
+(else_statement
+[
+  "else"
+  "end if"
+] @keyword.conditional)
+
+(for_statement
+[
+  "for"
+  "in"
+  "end for"
+] @keyword.repeat)
+
+(while_statement
+[
+  "while"
+  "end while"
+] @keyword.repeat)
+
+(function_definition
+[
+  "function"
+  "end function"
+] @keyword.function)
+
+;; Operators
 
 [
-  "="
-  "+="
-  "-="
-  "*="
-  "/="
-  "^="
-  "=="
-  "!="
-  "<"
-  ">"
-  "<="
-  ">="
+ "and"
+ "not"
+ "or"
+ "isa"
+] @keyword.operator
+
+[
   "+"
   "-"
   "*"
   "/"
   "%"
   "^"
+  "@"
+  "=="
+  "!="
+  "<"
+  ">"
+  "<="
+  ">="
 ] @operator
 
-;;; Punctuation
+;; Punctuations
 
-["," "."] @punctuation.delimiter
-["(" ")" "[" "]" "{" "}"] @punctuation.bracket
+[
+  ";"
+  ":"
+  ","
+  "."
+] @punctuation.delimiter
 
-;;; Variables
+;; Brackets
+
+[
+ "("
+ ")"
+ "["
+ "]"
+ "{"
+ "}"
+] @punctuation.bracket
+
+;; Variables
 
 (identifier) @variable
-(
-  (identifier) @variable.builtin
-  (#match? @variable.builtin "self")
-)
 
-;;; Constants
+((identifier) @variable.builtin
+ (#eq? @variable.builtin "self"))
+
+;; Constants
 
 (null) @constant.builtin
-[(false) (true)] @boolean
-(number) @number
 
-;;; Strings
+[
+  (false)
+  (true)
+] @boolean
 
-(string) @string
+;; Maps
 
-;;; Function calls
+(dot_index_expression index: (identifier) @variable.member)
+
+;; Functions
+
+(parameters name: (identifier) @variable.parameter)
+
+(assignment_statement
+  (variable .
+    [
+      (identifier) @function
+      (bracket_index_expression
+        index: (identifier) @function)
+      (dot_index_expression
+        index: (identifier) @function)
+    ])
+  (function_definition))
 
 (function_call
-  name: (identifier) @function.call
-  arguments: (arguments)?)
+  name: [
+    (identifier) @function.call
+    (bracket_index_expression
+      index: (identifier) @function.call)
+    (dot_index_expression
+      index: (identifier) @function.call)
+  ])
 
-;;; Function definitions
+;; (function_call
+;;   (identifier) @function.builtin
+;;   (#any-of? @function.builtin
+;;     ;; built-in functions in Miniscript
+;;     "assert" "collectgarbage" "dofile" "error" "getfenv" "getmetatable" "ipairs"
+;;     "load" "loadfile" "loadstring" "module" "next" "pairs" "pcall" "print"
+;;     "rawequal" "rawget" "rawset" "require" "select" "setfenv" "setmetatable"
+;;     "tonumber" "tostring" "type" "unpack" "xpcall"))
 
-(parameters) @parameter
-
-;;; Comments
+;; Others
 
 (comment) @comment
 
-;;; Errors
+(number) @number
 
-(ERROR) @error
+(string) @string
 
+(escape_sequence) @string.escape
